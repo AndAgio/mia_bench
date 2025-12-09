704cdbecbd046a62c86ed493bdb770da7ec86b08
AndAgio <aagiollo@gmail.com>
2025-12-09 16:34:16 +0100

    feat: making one run file flexible enough to run all mia attacks.

--- run.py
+++ run.py
@@ -4,11 +4,10 @@ from src.utils.configs import TrainConfigs, LogConfigs, ModelConfigs, DatasetCon
 from src.utils.log import get_logger_from_configs
 from src.mia.victim import Victim
 from src.mia.rmia import RMIA
+from src.mia.lira import LiRA
 
 
 def main():
-    # TODO: Make main adaptable to multiple MIAs.
-    # assignees: AndAgio.
     settings = gather_settings()
 
     exp_code = f'{settings.attack_mode}_{settings.dataset}_victim_{settings.victim_model}_{settings.victim_optimizer}_{settings.victim_lr}_{settings.victim_lr_sched}_{settings.victim_seed}_attacker_{settings.att_model}_{settings.att_optimizer}_{settings.att_lr}_{settings.att_lr_sched}_{settings.att_seed}'
@@ -67,36 +66,43 @@ def main():
                                                 seed=settings.att_seed)
     attacker_shadow_configs = ShadowDataConfigs(n_shadow_datasets=settings.n_shadows,
                                                 n_samples_per_dataset=settings.n_samples_per_shadow_dataset,
-                                                mode='offline',
+                                                mode='online',
                                                 test_perc=settings.shadow_test_perc,
                                                 seed=settings.att_seed)
 
-    attacker = RMIA(victim_model=victim_model,
-                    victim_dataset=victim.get_dataset(),
-                    audit_configs=attacker_audit_configs,
-                    shadow_configs=attacker_shadow_configs,
-                    model_configs=attacker_model_configs,
-                    logger=get_logger_from_configs(attacker_log_configs))
-    attacker.optimize(train_config=attacker_train_configs)
-    scores = attacker.measure_effectiveness(alpha=settings.rmia_alphas,
-                                            random_pop_size=settings.random_population_size,
-                                            device=attacker_train_configs.device)
-    # attacker.measure_effectiveness()
-    # auc_score, tpr, fpr, roc = attacker.get_stats(scores=scores)
-    for alpha, scores_dict in scores.items():
-        print(f'Alpha = {alpha} -> AUC = {scores_dict['auc']}')
+    if settings.attack_mode in ['online_rmia', 'offline_rmia', 'on_rmia', 'off_rmia']:
+        rmia_mode = 'online' if settings.attack_mode in ['online_rmia', 'on_rmia'] else 'offline'
+        attacker_shadow_configs.mode = rmia_mode
+        attacker = RMIA(victim_model=victim_model,
+                        victim_dataset=victim.get_dataset(),
+                        audit_configs=attacker_audit_configs,
+                        shadow_configs=attacker_shadow_configs,
+                        model_configs=attacker_model_configs,
+                        logger=get_logger_from_configs(attacker_log_configs))
+        attacker.optimize(train_config=attacker_train_configs)
+        attacker.measure_effectiveness(alpha=settings.rmia_alphas,
+                                    gamma=settings.rmia_gamma,
+                                    random_pop_size=settings.random_population_size,
+                                    device=attacker_train_configs.device)
+    elif settings.attack_mode == 'lira':
+        attacker = LiRA(victim_model=victim_model,
+                        victim_dataset=victim.get_dataset(),
+                        audit_configs=attacker_audit_configs,
+                        shadow_configs=attacker_shadow_configs,
+                        model_configs=attacker_model_configs,
+                        logger=get_logger_from_configs(attacker_log_configs))
+        attacker.optimize(train_config=attacker_train_configs)
+        attacker.measure_effectiveness(device=attacker_train_configs.device)
+    else:
+        raise ValueError('Attack "{}" not found or not implemented yet! Double check your settings please!'.format(settings.attack_mode))
 
-    # TODO: Find better way to store and deal with experiment results.
-    # assignees: AndAgio.
+    best_auc_params, best_auc = attacker.get_best_result('auc', mode='max')
+    print('Best AUC was obtained for paramters:{} and was {}'.format(best_auc_params, best_auc))
+    # print(attacker.summarize_results())
+
     exp_results_folder = settings.out_folder/f'{exp_code}'
     os.makedirs(exp_results_folder, exist_ok=True)
-    with open(os.path.join(exp_results_folder, 'results.txt'), 'w') as file:
-        for alpha, scores_dict in scores.items():
-            file.write(f'Alpha = {alpha} -> AUC: {scores_dict['auc']}\n')
-            file.write(f'Alpha = {alpha} -> TPR: {scores_dict['tpr']}\n')
-            file.write(f'Alpha = {alpha} -> FPR: {scores_dict['fpr']}\n')
-            file.write(f'Alpha = {alpha} -> ROC: {scores_dict['roc']}\n')
-            file.write('\n')
+    attacker.save_results_to_json(os.path.join(exp_results_folder, 'results.json'))
 
 
 if __name__ == '__main__':
