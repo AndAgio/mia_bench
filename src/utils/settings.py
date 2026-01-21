66ab4c9e823cb397788a5dd1ddd00611ecb622aa
AndAgio <aagiollo@gmail.com>
2026-01-21 08:27:21 +0100

    fix: small stupid issue with default attack mode

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -76,7 +76,7 @@ def gather_settings():
         
 
         # Shared MIA parameters
-        parser.add_argument("--attack_mode", default="robust",
+        parser.add_argument("--attack_mode", default="quantile",
                                 choices=['online_robust', 'offline_robust', 'on_robust', 'off_robust', "lira", "quantile",
                                         "neural_feat", "neural_prob", "neural_logit", 
                                         'rmia_loss', 'rmia_confidence', 'rmia_entropy', 
