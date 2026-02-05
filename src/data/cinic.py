5a81930e03fcac52cb0132f60ad8fb203686fb94
AndAgio <aagiollo@gmail.com>
2026-02-05 09:46:08 +0100

    chore: get cinic dataset to use the targets field to match the behaviour of other datasets.

--- src/data/cinic.py
+++ src/data/cinic.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from torchvision import datasets, transforms
 from torch.utils.data import Dataset, DataLoader
 import urllib.request
@@ -22,6 +23,7 @@ class Cinic10(Dataset):
         # Define dataset paths
         self.train_dir = os.path.join(root, 'train')
         self.test_dir = os.path.join(root, 'test')
+        data_path = self.train_dir if self.train else self.test_dir
 
         # Download the dataset if necessary
         if self.download:
@@ -29,9 +31,13 @@ class Cinic10(Dataset):
 
         # Load the dataset
         self.dataset = datasets.ImageFolder(
-            root=self.train_dir if self.train else self.test_dir,
+            root=data_path,
             transform=self.transform
         )
+
+        self.classes, self.class_to_idx = self._find_classes(data_path)
+        self.samples = self.make_dataset(data_path, self.class_to_idx)
+        self.targets = [s[1] for s in self.samples]
 
     def _default_transform(self):
         """Default transform for the CINIC-10 dataset."""
@@ -67,6 +73,70 @@ class Cinic10(Dataset):
             os.remove(filename)
             print("Download and extraction complete.")
 
+    def _find_classes(self, dir):
+        """Creates classes from the folder structure.
+
+        Args:
+            dir: (string) Root directory path.
+
+        Returns:
+            tuple: (classes, class_to_idx) where classes are relative to (dir),
+            and class_to_idx is a dictionary.
+        """
+
+        classes = []
+
+        dir = Path(dir)
+
+        for d in dir.rglob("*"):
+            if d.is_dir():
+                # Split path into strings 
+                parts = d.parts # type: tuple, strings
+                item = f"{parts[-2]}_{parts[-1]}"
+                classes.append(item)
+
+        classes.sort()
+        class_to_idx = {classes[i]: i for i in range(len(classes))}
+
+        return classes, class_to_idx
+
+    def _get_target(self, file_path):
+        """Returns a target_class from the parent and grandparent folders.
+
+        Args:
+            file_path: (string) path to the file.
+
+        Returns:
+            target_class: (string) target class for that file.
+        """
+
+        parts = file_path.parts
+        target_class = f"{parts[-3]}_{parts[-2]}"
+        return target_class
+
+    def make_dataset(self, dir, class_to_idx):
+        """Returns a list of image path, and target index
+
+        Args:
+            dir: (string) The path of each image sample
+            class_to_idx: (dict: string, int) Sorted classes, mapped to int
+
+        Returns:
+            images: (list of tuples) Path and mapped class for each sample
+        """
+
+        images = []
+
+        dir = Path(dir)
+
+        for d in dir.rglob("*.png"):
+            if not d.is_dir():
+                target = self._get_target(d)
+                item = (d, class_to_idx[target])
+                images.append(item)
+
+        return images
+
     def __len__(self):
         """Returns the number of samples in the dataset."""
         return len(self.dataset)
@@ -84,3 +154,6 @@ class Cinic10(Dataset):
 # cinic10_test = CINIC10Dataset(root_dir='./data/cinic10', train=False, download=True)
 # train_loader = cinic10_train.get_dataloader(batch_size=64, shuffle=True)
 # test_loader = cinic10_test.get_dataloader(batch_size=64, shuffle=False)
+
+# cinic10_train = Cinic10(root='./datas/cinic10', train=True, download=True)
+# print(cinic10_train.targets)
